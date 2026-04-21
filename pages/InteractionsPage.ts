42297ab1b7bd49cd49a4b380a21424c8848f6e80
EvgenyDantsov <adriano-sport@mail.ru>
2026-04-21 20:27:35 +0300

    Refactor: replace brittle text-based locators with robust selectors

--- pages/InteractionsPage.ts
+++ pages/InteractionsPage.ts
@@ -7,8 +7,8 @@ export class InteractionsPage {
 
     constructor(page: Page) {
         this.page = page;
-        this.droppableLink = page.locator('text=Droppable'); //ссылка на вкладку Droppable
-        this.draggableLink = page.locator('text=Dragabble'); //ссылка на вкладку Draggable
+        this.droppableLink = page.locator('a[href$="/droppable"]'); //ссылка на вкладку Droppable
+        this.draggableLink = page.locator('a[href$="/dragabble"]'); //ссылка на вкладку Draggable
     }
 
     async clickDroppable() {
@@ -18,4 +18,5 @@ export class InteractionsPage {
     async clickDraggable() {
         await this.draggableLink.click();
     }
+
 }
