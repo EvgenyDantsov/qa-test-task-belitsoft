37c6b1190e8cef8388fc25ec272806c77ee3a04e
EvgenyDantsov <adriano-sport@mail.ru>
2026-04-21 20:38:25 +0300

    Remove unused import

--- tests/elements.spec.ts
+++ tests/elements.spec.ts
@@ -1,6 +1,5 @@
 import { test, expect } from '@playwright/test';
 import { HomePage } from '../pages/HomePage';
-import { InteractionsPage } from '../pages/InteractionsPage';
 import { ElementsPage } from '../pages/ElementsPage';
 
 test.describe('Elements tab tests', () => {
