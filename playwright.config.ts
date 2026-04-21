ce687ff573cbfa7e44514d5e903bf81c8a90b2ab
EvgenyDantsov <adriano-sport@mail.ru>
2026-04-21 20:25:57 +0300

    Add enable trace viewer for visual test debugging

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,17 +1,18 @@
+/// <reference types="node" />
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  fullyParallel: true,
+  fullyParallel: true, //false, чтобы тесты выполнялись по очереди
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1, //process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    headless: true,   // включаем визуальный режим
-    viewport: { width: 1920, height: 1080 },
+    headless: false, //true,   // включаем визуальный режим
+    //viewport: { width: 1920, height: 1080 },
     launchOptions: {
-      //     args: ['--start-maximized'], // открытие окна сразу во весь экран
+           args: ['--start-maximized'], // открытие окна сразу во весь экран
       slowMo: 500,
     },
     actionTimeout: 5000,
